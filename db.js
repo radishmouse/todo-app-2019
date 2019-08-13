d83cba718449385dc025b16f6ef89dd89cee8daf
Chris Aquino <chris@digitalcrafts.com>
2019-08-13 14:49:05 -0400

    puts db calls into Todo model file

--- db.js
+++ db.js
@@ -14,39 +14,5 @@ console.log(process.env.DB_PORT);
 console.log(process.env.DB_NAME);
 
 console.log('yay you did the thing. wow.');
-// console.log(db);
-// db.any(`
-// select * from todos
-// `)
-//     .then((data) => {
-//         console.log('here is the data:');
-//         console.log(data);
-//     })
-//     .catch((error) => {
-//         console.log("UH OH.");
-//         console.log(error);
-//     })
 
-
-// When you want one and only one,
-// use the .one() method.
-// That way, if you don't find it,
-// it triggers the .catch().
-// This is better than doing an if/else
-// inside your .then().
-// .one() will throw an exception if it
-// gets anything but 1 and only 1 result.
-function getOne(id) {
-    db.one(`
-        select * from todos where id=$1
-    `, [id])
-    .then((data) => {
-        console.log('here is the data:');
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log("UH OH.");
-        console.log(error);
-    })
-}
-getOne(42);
+module.exports = db;
