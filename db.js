4ea4b1281ef7fe9bba5d7a74838a22d1fb55d419
Chris Aquino <chris@digitalcrafts.com>
2019-08-13 13:08:25 -0400

    adds initial files for db setup

---
+++ db.js
@@ -0,0 +1,13 @@
+// Import the dotenv module.
+// Call its `.config()` method
+require('dotenv').config();
+
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+});
+
+console.log('yay you did the thing. wow.');
+console.log(db);
