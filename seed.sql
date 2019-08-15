18b399cfb9bcd21d6dd9f708c512b013d374f5d4
Chris Aquino <chris@digitalcrafts.com>
2019-08-15 09:46:34 -0400

    adds user table and more seed data

--- seed.sql
+++ seed.sql
@@ -1,4 +1,22 @@
+insert into users
+    (displayname, username)
+values
+    ('alice', 'l33th4x0r'),
+    ('bob', 'puppy_lover')
+;
+
+select * from users;
+
 insert into todos 
-    (priority, task)
+    (priority, task, user_id)
 values 
-    (1, 'feed the cat');
+    (1, 'feed the cat', 1),
+    (2, 'pet the cat', 1),
+    (3, 'worship the cat', 1),
+    (99, 'go to work', 1),
+    (1, 'feed the dog', 2),
+    (2, 'pet the dog', 2),
+    (3, 'walk the dog', 2)
+;
+
+select * from todos;
